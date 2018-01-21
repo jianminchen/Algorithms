26adba3f686228cdaf48aa1a0ef4a553951ca4b8
Vikash Jain <vikashj@live.com>
2018-01-21 09:08:59 -0800

    Algorithm to create BST from sorted Array

---
+++ Algorithms/Trees_Graphs/BSTFromSortedArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Trees_Graphs
+{
+    class BSTFromSortedArray
+    {
+        private class Node
+        {
+            public int Value { get; set; }
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+        }
+
+        private static Node CreateBST(int[] input, int start, int end)
+        {
+            if (start > end)
+            {
+                return null;
+            }
+
+            int mid = (start + end) / 2;
+            Node midNode = new Node();
+            midNode.Value = input[mid];
+            midNode.LeftChild = CreateBST(input, start, mid - 1);
+            midNode.RightChild = CreateBST(input, mid + 1, end);
+
+            return midNode;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Node root = CreateBST(input, 0, 8);
+
+            Console.ReadKey();
+        }
+    }
+}
--- Algorithms/Trees_Graphs/RouteBetweenNodesInGraph.cs
+++ Algorithms/Trees_Graphs/RouteBetweenNodesInGraph.cs
@@ -33,41 +33,41 @@ namespace Algorithms.Trees_Graphs
             }
         }
 
-        public static void Main(string[] args)
-        {
-            Graph graph = new Graph();
+        //public static void Main(string[] args)
+        //{
+        //    Graph graph = new Graph();
 
-            GraphNode node1 = new GraphNode(1);
-            GraphNode node2 = new GraphNode(2);
-            GraphNode node3 = new GraphNode(3);
-            GraphNode node4 = new GraphNode(4);
-            GraphNode node5 = new GraphNode(5);
+        //    GraphNode node1 = new GraphNode(1);
+        //    GraphNode node2 = new GraphNode(2);
+        //    GraphNode node3 = new GraphNode(3);
+        //    GraphNode node4 = new GraphNode(4);
+        //    GraphNode node5 = new GraphNode(5);
 
-            node1.AdjacentNodes.Add(node2);
-            node1.AdjacentNodes.Add(node3);
+        //    node1.AdjacentNodes.Add(node2);
+        //    node1.AdjacentNodes.Add(node3);
 
-            node2.AdjacentNodes.Add(node4);
+        //    node2.AdjacentNodes.Add(node4);
 
-            node4.AdjacentNodes.Add(node5);
-            node4.AdjacentNodes.Add(node3);
-            node5.AdjacentNodes.Add(node1);
-            graph.Nodes.Add(node1);
-            graph.Nodes.Add(node2);
-            graph.Nodes.Add(node3);
-            graph.Nodes.Add(node4);
-            graph.Nodes.Add(node5);
+        //    node4.AdjacentNodes.Add(node5);
+        //    node4.AdjacentNodes.Add(node3);
+        //    node5.AdjacentNodes.Add(node1);
+        //    graph.Nodes.Add(node1);
+        //    graph.Nodes.Add(node2);
+        //    graph.Nodes.Add(node3);
+        //    graph.Nodes.Add(node4);
+        //    graph.Nodes.Add(node5);
 
-            /*
-             * 1 -> 2, 3
-             * 2 -> 4
-             * 4 -> 3,5
-             * 5 -> 1
-             * */
+        //    /*
+        //     * 1 -> 2, 3
+        //     * 2 -> 4
+        //     * 4 -> 3,5
+        //     * 5 -> 1
+        //     * */
 
-            bool result = Search(graph, node4, node1);
-            Console.WriteLine(result);
-            Console.ReadKey();
-        }
+        //    bool result = Search(graph, node4, node1);
+        //    Console.WriteLine(result);
+        //    Console.ReadKey();
+        //}
 
         static bool Search(Graph graph, GraphNode start, GraphNode end)
         {
