f7746463c4ff4316292680ce9962cc0224584983
Vikash Jain <vikashj@live.com>
2018-01-25 08:05:02 -0800

    Budget allocation problem

---
+++ Algorithms/MockInterviews/BudgetAllocation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.MockInterviews
+{
+    /*
+     * The awards committee of your alma mater (i.e. your college/university) asked for your assistance with a budget allocation problem they’re facing. Originally, the committee planned to give N research grants this year. However, due to spending cutbacks, the budget was reduced to newBudget dollars and now they need to reallocate the grants. The committee made a decision that they’d like to impact as few grant recipients as possible by applying a maximum cap on all grants. Every grant initially planned to be higher than cap will now be exactly cap dollars. Grants less or equal to cap, obviously, won’t be impacted.
+       Given an array grantsArray of the original grants and the reduced budget newBudget, write a function findGrantsCap that finds in the most efficient manner a cap such that the least number of recipients is impacted and that the new budget constraint is met (i.e. sum of the N reallocated grants equals to new
+     * */
+    class BudgetAllocation
+    {
+        private static double FindGrantsCap(double[] input, double newBudget)
+        {
+            double currentBudget = 0;
+            foreach(var currentGrant in input)
+            {
+                currentBudget += currentGrant;
+            }
+
+            int n = input.Length;
+            Array.Sort(input);
+            double[] newInput = new double[input.Length + 1];
+            for(int i = 0;i<n; i++)
+            {
+                newInput[i] = input[n - 1 - i];
+            }
+            newInput[n] = 0;
+
+            double surplus = currentBudget - newBudget;
+
+            if(surplus <= 0)
+            {
+                return input[n - 1];
+            }
+
+            int count = 0;
+            for (int i =0; i <n;i++)
+            {
+                surplus -= (i + 1) * (newInput[i] - newInput[i + 1]);
+                if (surplus <= 0)
+                    break;
+                count++;
+            }
+            return newInput[count+1] + (-surplus / (count + 1));
+        }
+
+        public static void Main(string[] args)
+        {
+            double[] input = new double[] { 2, 100, 50, 120, 1000 };
+            double newBudget = 190;
+            var result = FindGrantsCap(input, 190);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
--- Algorithms/Trees_Graphs/ValidateBST.cs
+++ Algorithms/Trees_Graphs/ValidateBST.cs
@@ -39,51 +39,51 @@ namespace Algorithms.Trees_Graphs
             }
         }
 
-        public static void Main(string[] args)
-        {
-            // Positive case
-            TreeNode root = new TreeNode(10);
-            TreeNode l1Node1 = new TreeNode(5);
-            TreeNode l1Node2 = new TreeNode(20);
-            root.Left = l1Node1;
-            root.Right = l1Node2;
+        //public static void Main(string[] args)
+        //{
+        //    // Positive case
+        //    TreeNode root = new TreeNode(10);
+        //    TreeNode l1Node1 = new TreeNode(5);
+        //    TreeNode l1Node2 = new TreeNode(20);
+        //    root.Left = l1Node1;
+        //    root.Right = l1Node2;
 
-            TreeNode l2Node1 = new TreeNode(3);
-            TreeNode l2Node2 = new TreeNode(7);
-            TreeNode l2Node3 = new TreeNode(15);
-            TreeNode l2Node4 = new TreeNode(40);
+        //    TreeNode l2Node1 = new TreeNode(3);
+        //    TreeNode l2Node2 = new TreeNode(7);
+        //    TreeNode l2Node3 = new TreeNode(15);
+        //    TreeNode l2Node4 = new TreeNode(40);
 
-            root.Left.Left = l2Node1;
-            root.Left.Right = l2Node2;
-            root.Right.Left = l2Node3;
-            root.Right.Right = l2Node4;
+        //    root.Left.Left = l2Node1;
+        //    root.Left.Right = l2Node2;
+        //    root.Right.Left = l2Node3;
+        //    root.Right.Right = l2Node4;
 
-            bool result = IsBST(root, Int32.MinValue, Int32.MaxValue);
+        //    bool result = IsBST(root, Int32.MinValue, Int32.MaxValue);
 
-            Console.WriteLine(result);
-            Console.ReadKey();
+        //    Console.WriteLine(result);
+        //    Console.ReadKey();
 
-            // Negative case
-            TreeNode secondRoot = new TreeNode(10);
-            TreeNode secondl1Node1 = new TreeNode(5);
-            TreeNode secondl1Node2 = new TreeNode(15);
-            secondRoot.Left = secondl1Node1;
-            secondRoot.Right = secondl1Node2;
+        //    // Negative case
+        //    TreeNode secondRoot = new TreeNode(10);
+        //    TreeNode secondl1Node1 = new TreeNode(5);
+        //    TreeNode secondl1Node2 = new TreeNode(15);
+        //    secondRoot.Left = secondl1Node1;
+        //    secondRoot.Right = secondl1Node2;
 
-            TreeNode secondl2Node1 = new TreeNode(3);
-            TreeNode secondl2Node2 = new TreeNode(12);
+        //    TreeNode secondl2Node1 = new TreeNode(3);
+        //    TreeNode secondl2Node2 = new TreeNode(12);
 
-            secondRoot.Left.Left = secondl2Node1;
-            secondRoot.Left.Right = secondl2Node2;
+        //    secondRoot.Left.Left = secondl2Node1;
+        //    secondRoot.Left.Right = secondl2Node2;
 
-            TreeNode secondl3Node1 = new TreeNode(1);
+        //    TreeNode secondl3Node1 = new TreeNode(1);
 
-            secondRoot.Left.Left.Left = secondl3Node1;
+        //    secondRoot.Left.Left.Left = secondl3Node1;
 
-            result = IsBST(secondRoot, Int32.MinValue, Int32.MaxValue);
+        //    result = IsBST(secondRoot, Int32.MinValue, Int32.MaxValue);
 
-            Console.WriteLine(result);
-            Console.ReadKey();
-        }
+        //    Console.WriteLine(result);
+        //    Console.ReadKey();
+        //}
     }
 }
