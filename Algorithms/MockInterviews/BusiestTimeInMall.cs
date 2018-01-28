3a9c80fbf279c2522a99fca4c1fa237a2edec239
Vikash Jain <vikashj@live.com>
2018-01-28 11:11:53 -0800

    Algorithm to find minimum number of platforms needed at railway station

--- Algorithms/MockInterviews/BusiestTimeInMall.cs
+++ Algorithms/MockInterviews/BusiestTimeInMall.cs
@@ -67,21 +67,21 @@ output: 1487800378 # since the increase in the number of people
             return busiestTime;
         }
 
-        static void Main(string[] args)
-        {
-            int[,] input = new int[,]{{1487799425, 14, 1 },
-                 { 1487799425, 4, 0 },
-                 { 1487799425, 2, 0 },
-                 { 1487800378, 10, 1 },
-                 { 1487801478, 18, 0 },
-                 { 1487801478, 18, 1 },
-                 { 1487901013, 1,  0 },
-                 { 1487901211, 7,  1 },
-                 { 1487901211, 7,  0 }};
+        //static void Main(string[] args)
+        //{
+        //    int[,] input = new int[,]{{1487799425, 14, 1 },
+        //         { 1487799425, 4, 0 },
+        //         { 1487799425, 2, 0 },
+        //         { 1487800378, 10, 1 },
+        //         { 1487801478, 18, 0 },
+        //         { 1487801478, 18, 1 },
+        //         { 1487901013, 1,  0 },
+        //         { 1487901211, 7,  1 },
+        //         { 1487901211, 7,  0 }};
 
-            var busiestTime = FindBusiestPeriod(input);
-            Console.WriteLine(busiestTime);
-            Console.ReadKey();
-        }
+        //    var busiestTime = FindBusiestPeriod(input);
+        //    Console.WriteLine(busiestTime);
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ Algorithms/MockInterviews/MinimumNumberOfPlatforms.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.MockInterviews
+{
+    /*
+     * Minimum Number of Platforms Required for a Railway/Bus Station
+3.2
+Given arrival and departure times of all trains that reach a railway station, find the minimum number of platforms required for the railway station so that no train waits.
+We are given two arrays which represent arrival and departure times of trains that stop
+Examples:
+Input:  arr[]  = {9:00,  9:40, 9:50,  11:00, 15:00, 18:00}
+        dep[]  = {9:10, 12:00, 11:20, 11:30, 19:00, 20:00}
+Output: 3
+There are at-most three trains at a time (time between 11:00 to 11:20)
+     * */
+
+    class MinimumNumberOfPlatforms
+    {
+        private static int GetMinimumPlatforms(int[] arr, int[] dep)
+        {
+            if(arr == null || dep == null || arr.Length != dep.Length)
+            {
+                return -1;
+            }
+
+            Array.Sort(arr);
+            Array.Sort(dep);
+
+            int i = 0;
+            int j = 0;
+            int maxPlatforms = 0;
+            int totalTrains = 0;
+            while(i < arr.Length)
+            {
+                if(arr[i] < dep[j])
+                {
+                    i++;
+                    totalTrains++;
+                }
+                else
+                {
+                    j++;
+                    totalTrains--;
+                }
+
+                if(totalTrains > maxPlatforms)
+                {
+                    maxPlatforms = totalTrains;
+                }
+            }
+
+            return maxPlatforms;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 900, 940, 950, 1100, 1500, 1800 };
+            int[] dep = new int[] { 910, 1200, 1120, 1130, 1900, 2000 };
+
+            int minimumPlatforms = GetMinimumPlatforms(arr, dep);
+            Console.WriteLine(minimumPlatforms);
+            Console.ReadKey();
+        }
+    }
+}
